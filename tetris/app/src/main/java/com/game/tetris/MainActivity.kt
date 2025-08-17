807e980ebbbf4bc6983e51616ba9af5366aaec6f
kohei-mu <kendo7bharat@gmail.com>
2025-08-17 17:31:40 +0900

    Keep Resume button visible after use

--- tetris/app/src/main/java/com/game/tetris/MainActivity.kt
+++ tetris/app/src/main/java/com/game/tetris/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         btnResume.setOnClickListener {
             paused = false
             gameView.setPaused(false)
-            btnResume.visibility = View.GONE
         }
 
         val btnDrop: Button = findViewById(R.id.btnDrop)
